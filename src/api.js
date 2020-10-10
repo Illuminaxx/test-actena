4c6fc5f3222142bf7b74de3d5643c690e048499c
Illuminaxx <nexgen62@hotmail.fr>
2020-10-10 18:05:15 +0200

    reverse env

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 let api = axios.create({
   headers: {
-    "client-id": process.env.REACT_APP_CLIENT_ID,
+    "client-id": "xtioorlp1zygrslbhjni5cy06zvxfw",
     "Authorization": "Bearer 18cug86hcuzxvgrf022hem88iumoig"
   }
 });
