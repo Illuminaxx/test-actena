d02880f12602764a993dbaa075d6946f8eb8bbf4
Illuminaxx <nexgen62@hotmail.fr>
2020-10-10 17:57:32 +0200

    process env and git ignore

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 let api = axios.create({
   headers: {
-    "client-id": "xtioorlp1zygrslbhjni5cy06zvxfw",
-    "Authorization": "Bearer 18cug86hcuzxvgrf022hem88iumoig"
+    "client-id": process.env.REACT_APP_CLIENT_ID,
+    "Authorization": process.env.REACT_APP_TOKEN
   }
 });
 
